50f593251324b9790e782ab7fec8d61924d040d8
Armando Alvear <armando.alvear01@outlook.com>
2024-02-25 10:06:01 -0600

    install and add async dispatch function fetchUsers

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -1,7 +1,40 @@
-import React from "react";
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../redux";
 
-function UserContainer() {
-  return <div>UserContainer</div>;
+function UserContainer({ userData, fetchUsers }) {
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  console.log(userData);
+
+  return userData.isLoading ? (
+    <h2>Loading...</h2>
+  ) : userData.error ? (
+    <h2>{userData.error}</h2>
+  ) : (
+    <div>
+      <h2>Users</h2>
+      <ul>
+        {userData &&
+          userData.users &&
+          userData.users.map((user) => <li>{user.name}</li>)}
+      </ul>
+    </div>
+  );
 }
 
-export default UserContainer;
+const mapStateToProps = (state) => {
+  return {
+    userData: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
